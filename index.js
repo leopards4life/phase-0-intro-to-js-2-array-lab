b6d9e2116172160c3163192c85b4e8d93364d70a
Jessica Vaughn <jessica.vaughn619@gmail.com>
2022-11-18 12:58:22 -0700

    Solution

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const allCats = [...cats, name];
+    return allCats;
+}
+
+function prependCat(name) {
+    const moreCats = [name, ...cats];
+    return moreCats;
+}
+
+function removeLastCat() {
+    const lessCats = cats.slice(0, cats.length - 1);
+    return lessCats;
+}
+
+function removeFirstCat() {
+    const lessKitties = cats.slice(1);
+    return lessKitties;
+}
